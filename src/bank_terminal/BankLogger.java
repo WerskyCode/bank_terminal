7f130c8e8c63a4ec598cc02b9e4403d7820cfbab
WerskyCode <werskycode@mail.ru>
2026-09-04 21:24:07 +0500

    Добавил метод printBalanceReport с varargs и обновил логирование успехов

--- src/bank_terminal/BankLogger.java
+++ src/bank_terminal/BankLogger.java
@@ -7,17 +7,28 @@ public class BankLogger {
     private static final DateTimeFormatter formattedPattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static void logTransferError(Exception e) {
-        LocalDateTime now = (LocalDateTime.now());
+        LocalDateTime now = LocalDateTime.now();
         String formattedDate = now.format(formattedPattern);
         System.out.println("ОШИБКА ПЕРЕВОДА!");
-        System.out.println("Время " + formattedDate);
-        System.out.println("Причина " + e.getMessage());
+        System.out.println("Время: " + formattedDate);
+        System.out.println("Причина: " + e.getMessage());
         System.out.println();
     }
 
-    public static void logTransferAccess(Account account) {
-        System.out.println("Перевод выполнен успешно");
-        System.out.println("Ваш баланс "  + account.getBalance());
+    public static void logTransferSuccess(double amount) {
+        LocalDateTime now = LocalDateTime.now();
+        String formattedDate = now.format(formattedPattern);
+        System.out.println("Перевод выполнен успешно!");
+        System.out.println("Сумма перевода " + amount);
+        System.out.println("Время: " + formattedDate);
+        System.out.println();
+    }
+
+    public static void printBalanceReport(Account ... accounts) {
+        System.out.println("         ---Балансы---");
+        for (Account account : accounts) {
+            System.out.printf("ID: %10s | Balance: %10.2f\n",account.getAccountNumber(), account.getBalance());
+        }
         System.out.println();
     }
 
--- src/bank_terminal/Main.java
+++ src/bank_terminal/Main.java
@@ -2,49 +2,71 @@ package bank_terminal;
 
 public class Main {
     public static void main(String[] args) {
+        // Создаем аккаунты 2-х видов
         SavingsAccount savingsAccount = new SavingsAccount("123456", 20000);
         CreditAccount creditAccount = new CreditAccount("123", 10000, 20000);
         Bank bank = new Bank();
 
+        final double ZERO = 0;
+        final double _5K = 5000;
+        final double _500K = 500000;
+        final double N5K = -5000;
+
+        // 1. Перевод 5000 со сберегательного счета на кредитный.
+        // Успешный перевод
         try {
-            bank.transfer(savingsAccount, creditAccount, 5000);
-            BankLogger.logTransferAccess(savingsAccount);
+            bank.transfer(savingsAccount, creditAccount, _5K);
+            BankLogger.logTransferSuccess(_5K);
         } catch (Exception e) {
             BankLogger.logTransferError(e);
         }
 
+        // 2. Перевод 500000 со сберегательного счета на кредитный.
+        // Ошибка количества средств
         try {
-            bank.transfer(savingsAccount, creditAccount, 500000);
-            System.out.println(savingsAccount.getBalance());
+            bank.transfer(savingsAccount, creditAccount, _500K);
+            BankLogger.logTransferSuccess(_500K);
         } catch (Exception e) {
             BankLogger.logTransferError(e);
         }
 
+        // 3. Перевод 500000 с кредитного счета на сберегательный.
+        // Ошибка количества средств
         try {
-            bank.transfer(creditAccount, savingsAccount, 500000);
-            System.out.println(creditAccount.getBalance());
+            bank.transfer(creditAccount, savingsAccount, _500K);
+            BankLogger.logTransferSuccess(_500K);
         } catch (Exception e) {
             BankLogger.logTransferError(e);
         }
 
+        // 4. Перевод -5000 со сберегательного счета на кредитный.
+        // Ошибка неправильного ввода данных
         try {
-            bank.transfer(savingsAccount, creditAccount, -5000);
-            System.out.println(savingsAccount.getBalance());
-            System.out.println(creditAccount.getBalance());
+            bank.transfer(savingsAccount, creditAccount, N5K);
+            BankLogger.logTransferSuccess(N5K);
         } catch (Exception e) {
             BankLogger.logTransferError(e);
         }
-        System.out.println(savingsAccount.getBalance());
-        System.out.println(creditAccount.getBalance());
 
+        // 4. Перевод -5000 с кредитного счета на сберегательный.
+        // Ошибка неправильного ввода данных
         try {
-            bank.transfer(creditAccount, savingsAccount, -5000);
-            System.out.println(savingsAccount.getBalance());
-            System.out.println(creditAccount.getBalance());
+            bank.transfer(creditAccount, savingsAccount, N5K);
+            BankLogger.logTransferSuccess(N5K);
         } catch (Exception e) {
             BankLogger.logTransferError(e);
         }
-        System.out.println(savingsAccount.getBalance());
-        System.out.println(creditAccount.getBalance());
+
+        // 5. Перевод 0 со сберегательного счета на кредитный.
+        // Ошибка неправильного ввода данных
+        try {
+            bank.transfer(savingsAccount, creditAccount, ZERO);
+            BankLogger.logTransferSuccess(ZERO);
+        } catch (Exception e) {
+            BankLogger.logTransferError(e);
+        }
+
+        // Вывод балансов всех аккаунтов
+        BankLogger.printBalanceReport(savingsAccount, creditAccount);
     }
 }
